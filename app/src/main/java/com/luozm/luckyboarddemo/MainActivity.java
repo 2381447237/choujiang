10ecf38d0ea3dd37b8a1dfe5da874f34174c2371
2381447237 <2381447237@qq.com>
2018-04-16 15:52:15 +0800

    首次提交

---
+++ app/src/main/java/com/luozm/luckyboarddemo/MainActivity.java
@@ -0,0 +1,54 @@
+package com.luozm.luckyboarddemo;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.widget.Toast;
+
+import com.luozm.luckyboard.LuckyAward;
+import com.luozm.luckyboard.LuckyBoard;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    LuckyBoard luckyBoard;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+        luckyBoard = (LuckyBoard) findViewById(R.id.luckyboard);
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher);
+        Bitmap ake = BitmapFactory.decodeResource(getResources(), R.drawable.ake);
+        Bitmap bailishouyue = BitmapFactory.decodeResource(getResources(), R.drawable.bailishouyue);
+        Bitmap caocao = BitmapFactory.decodeResource(getResources(), R.drawable.caocao);
+        Bitmap huangzhong = BitmapFactory.decodeResource(getResources(), R.drawable.huangzhong);
+        Bitmap liubei = BitmapFactory.decodeResource(getResources(), R.drawable.liubei);
+        Bitmap xiahoudun = BitmapFactory.decodeResource(getResources(), R.drawable.xiahoudun);
+        Bitmap zhangfei = BitmapFactory.decodeResource(getResources(), R.drawable.zhangfei);
+        Bitmap zhaoyun = BitmapFactory.decodeResource(getResources(), R.drawable.zhaoyun);
+        List<LuckyAward> awards = new ArrayList<>();
+        awards.add(new LuckyAward("阿珂", ake, 0.1f));//表示中奖的概率（范围在0到1之间）
+        awards.add(new LuckyAward("百里守约", bailishouyue, 0.1f));
+        awards.add(new LuckyAward("曹操", caocao, 0.1f));
+        awards.add(new LuckyAward("黄忠", huangzhong, 0.1f));
+        awards.add(new LuckyAward("刘备", liubei, 0.1f));
+        awards.add(new LuckyAward("夏侯惇", xiahoudun, 0.1f));
+        awards.add(new LuckyAward("张飞", zhangfei, 0.1f));
+        awards.add(new LuckyAward("赵云", zhaoyun, 0.1f));
+        luckyBoard.setAvAward(new LuckyAward("谢谢惠顾", bitmap, 0f));
+        luckyBoard.setAwards(awards);
+        luckyBoard.setResultCallback(new LuckyBoard.ResultCallback() {
+            @Override
+            public void result(LuckyAward award) {
+                Toast.makeText(MainActivity.this, award.getName(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+}
